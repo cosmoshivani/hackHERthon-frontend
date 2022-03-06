485270104d3b3e1ea8d4b12dd12f89294e6c029b
Shivani Mishra <47838688+cosmoshivani@users.noreply.github.com>
2022-03-06 13:32:04 +0530

    Update About.js

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -3,12 +3,43 @@ import {Wrapper, Content} from './About-styles';
 const About = () => {
     return(
         <Wrapper>
+            <h1>
+            Safety shouldn’t come with barriers.
+            That’s why we’re here to help remove them.
+            </h1>
+            <h2>
+            One billion people, 15 &percnt; of the world's population, experience some form of disability.
+            </h2>
+            <h4>
+            In the event of a crisis or disaster, people with disabilities are more likely to be left behind.
+            </h4>
+            <h5>
+            Evac-Ability aims to knock down the barriers that block people with disabilities from accessing 
+            their right to safety. We spoke to real people - from the mother of a non-speaking autistic child, 
+            to a young adult with sensory and mobility issues. They wanted a way to access relevant, 
+            reliable safety information during times of crisis.
+    
+            </h5>
+            <p>
+            We connect people with disabilities to the information they need, right where they are:
+            </p>
+            <p>
+            Nearby emergency shelters Accessible restroom locations Food pantries/distribution Delivery of medicines, 
+            medical equipment, vitamins, and hygiene products
+    
+            </p>
+            <h3>
+            Some people are have difficulty accessing essential resources because of their disability.
+            </h3>
+
             
-            <Content>
+            
            
-            </Content>
+
         </Wrapper>
     )
 }
 
 export default About;
+
+
