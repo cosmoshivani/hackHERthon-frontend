a9beb83143bab413fba95f871842ed9c43ce310d
Shivani Mishra <47838688+cosmoshivani@users.noreply.github.com>
2022-03-06 13:30:32 +0530

    Update About-styles.js

--- src/pages/About/About-styles.js
+++ src/pages/About/About-styles.js
@@ -5,9 +5,443 @@ export const Wrapper = styled.div`
     h1 {
         letter-spacing: 1px;
     }
-    
-`;
+   body{
+        background-color: E5E5E5;
+        
+    }
 
-export const Content = styled.div`
-    margin: 50px;
+    /* About Page Hi-Fi */
+
+    position: relative;
+    width: 1440px;
+    height: 1024px;
+    
+    background: #FAFAFD;
+    
+    
+    /* Shelter */
+    
+    position: absolute;
+    width: 90px;
+    height: 39px;
+    left: 942px;
+    top: 52px;
+    
+    font-family: Poppins;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 26px;
+    line-height: 39px;
+    /* identical to box height */
+    display: flex;
+    align-items: center;
+    
+    color: #002E5B;
+    
+    
+    
+    /* Resources */
+    
+    position: absolute;
+    width: 134px;
+    height: 39px;
+    left: 726px;
+    top: 53px;
+    
+    font-family: Poppins;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 26px;
+    line-height: 39px;
+    /* identical to box height */
+    display: flex;
+    align-items: center;
+    
+    color: #002E5B;
+    
+    
+    
+    /* About */
+    
+    position: absolute;
+    width: 82px;
+    height: 39px;
+    left: 1097px;
+    top: 52px;
+    
+    font-family: Poppins;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 26px;
+    line-height: 39px;
+    /* identical to box height */
+    display: flex;
+    align-items: center;
+    
+    color: #002E5B;
+    
+    
+    
+    /* Donate */
+    
+    position: absolute;
+    width: 95px;
+    height: 39px;
+    left: 1266px;
+    top: 52px;
+    
+    font-family: Poppins;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 26px;
+    line-height: 39px;
+    /* identical to box height */
+    display: flex;
+    align-items: center;
+    
+    color: #002E5B;
+    
+    
+    
+    /* Group 47 */
+    
+    position: absolute;
+    width: 243px;
+    height: 49.87px;
+    left: 76px;
+    top: 42px;
+    
+    
+    
+    /* Group 46 */
+    
+    position: absolute;
+    width: 243px;
+    height: 49.87px;
+    left: 76px;
+    top: 42px;
+    
+    
+    
+    /* EVA BILITY */
+    
+    position: absolute;
+    width: 243px;
+    height: 30.89px;
+    left: 76px;
+    top: 56.72px;
+    
+    background: #002E5B;
+    
+    
+    /* Group 45 */
+    
+    position: absolute;
+    width: 31.21px;
+    height: 37.03px;
+    left: 189.07px;
+    top: 49.06px;
+    
+    
+    
+    /* Vector */
+    
+    position: absolute;
+    left: 13.92%;
+    right: 85.49%;
+    top: 4.79%;
+    bottom: 94.42%;
+    
+    background: #002E5B;
+    
+    
+    /* Vector */
+    
+    position: absolute;
+    left: 13.13%;
+    right: 84.7%;
+    top: 5.69%;
+    bottom: 91.59%;
+    
+    background: #002E5B;
+    
+    
+    /* Vector */
+    
+    position: absolute;
+    left: 10.41%;
+    right: 87.06%;
+    top: 4.1%;
+    bottom: 91.83%;
+    
+    background: #002E5B;
+    transform: rotate(15.32deg);
+    
+    
+    /* - */
+    
+    position: absolute;
+    width: 7.84px;
+    height: 3.45px;
+    left: 179.65px;
+    top: 70.64px;
+    
+    background: #002E5B;
+    
+    
+    /* Rectangle 204 */
+    
+    position: absolute;
+    width: 324px;
+    height: 121px;
+    left: 50px;
+    top: 0px;
+    
+    
+    
+    /* Safety shouldn’t come with barriers. That’s why we’re here to help remove them. */
+    
+    position: absolute;
+    width: 797px;
+    height: 108px;
+    left: 330px;
+    top: 218px;
+    
+    font-family: Poppins;
+    font-style: normal;
+    font-weight: 600;
+    font-size: 36px;
+    line-height: 54px;
+    text-align: center;
+    
+    color: #002E5B;
+    
+    
+    
+    /* How do we do this? */
+    
+    position: absolute;
+    width: 348px;
+    height: 54px;
+    left: 546px;
+    top: 632px;
+    
+    font-family: Poppins;
+    font-style: normal;
+    font-weight: 600;
+    font-size: 36px;
+    line-height: 54px;
+    /* identical to box height */
+    text-align: center;
+    
+    color: #002E5B;
+    
+    
+    
+    /* Group 55 */
+    
+    position: absolute;
+    width: 390px;
+    height: 248.09px;
+    left: 76px;
+    top: 356px;
+    
+    
+    
+    /* Group 51 */
+    
+    position: absolute;
+    width: 390px;
+    height: 248.09px;
+    left: 76px;
+    top: 356px;
+    
+    
+    
+    /* Rectangle 168 */
+    
+    position: absolute;
+    width: 390px;
+    height: 248.09px;
+    left: 76px;
+    top: 356px;
+    
+    background: #034F96;
+    box-shadow: 0px 5px 15px rgba(0, 0, 0, 0.15);
+    border-radius: 15px;
+    
+    
+    /* One billion people, 15% of the world's population, experience some form of disability. */
+    
+    position: absolute;
+    width: 326px;
+    height: 156px;
+    left: 108px;
+    top: 402px;
+    
+    font-family: Poppins;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 26px;
+    line-height: 39px;
+    display: flex;
+    align-items: center;
+    text-align: center;
+    
+    color: #FAFAFD;
+    
+    
+    
+    /* Group 56 */
+    
+    position: absolute;
+    width: 390px;
+    height: 248.09px;
+    left: 520px;
+    top: 356px;
+    
+    
+    
+    /* Group 52 */
+    
+    position: absolute;
+    width: 390px;
+    height: 248.09px;
+    left: 520px;
+    top: 356px;
+    
+    
+    
+    /* Rectangle 168 */
+    
+    position: absolute;
+    width: 390px;
+    height: 248.09px;
+    left: 520px;
+    top: 356px;
+    
+    background: #034F96;
+    box-shadow: 0px 5px 15px rgba(0, 0, 0, 0.15);
+    border-radius: 15px;
+    
+    
+    /* In the event of a crisis or disaster, people with disabilities are more likely to be left behind. */
+    
+    position: absolute;
+    width: 347px;
+    height: 156px;
+    left: 547px;
+    top: 402px;
+    
+    font-family: Poppins;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 26px;
+    line-height: 39px;
+    display: flex;
+    align-items: center;
+    text-align: center;
+    
+    color: #FAFAFD;
+    
+    
+    
+    /* Evac-Ability aims to knock down the barriers that block people with disabilities from accessing their right to safety. We spoke to real people - from the mother of a non-speaking autistic child, to a young adult with sensory and mobility issues. They wanted a way to access relevant, reliable safety information during times of crisis. */
+    
+    position: absolute;
+    width: 596px;
+    height: 221px;
+    left: 89px;
+    top: 713px;
+    
+    font-family: Poppins;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 20px;
+    line-height: 30px;
+    
+    color: #000000;
+    
+    
+    
+    /* We connect people with disabilities to the information they need, right where they are: */
+    
+    position: absolute;
+    width: 453px;
+    height: 70px;
+    left: 772px;
+    top: 709px;
+    
+    font-family: Poppins;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 20px;
+    line-height: 30px;
+    
+    color: #000000;
+    
+    
+    
+    /* Nearby emergency shelters Accessible restroom locations Food pantries/distribution Delivery of medicines, medical equipment, vitamins, and hygiene products */
+    
+    position: absolute;
+    width: 454px;
+    height: 167px;
+    left: 763px;
+    top: 776px;
+    
+    font-family: Poppins;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 20px;
+    line-height: 30px;
+    
+    color: #000000;
+    
+    
+    
+    /* Group 57 */
+    
+    position: absolute;
+    width: 390px;
+    height: 248.09px;
+    left: 964px;
+    top: 356px;
+    
+    
+    
+    /* Rectangle 168 */
+    
+    position: absolute;
+    width: 390px;
+    height: 248.09px;
+    left: 964px;
+    top: 356px;
+    
+    background: #034F96;
+    box-shadow: 0px 5px 15px rgba(0, 0, 0, 0.15);
+    border-radius: 15px;
+    
+    
+    /* Some people are have difficulty accessing essential resources because of their disability. */
+    
+    position: absolute;
+    width: 341px;
+    height: 195px;
+    left: 990px;
+    top: 383px;
+    
+    font-family: Poppins;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 26px;
+    line-height: 39px;
+    display: flex;
+    align-items: center;
+    text-align: center;
+    
+    color: #FAFAFD;
+   
+    
+    
 `;
